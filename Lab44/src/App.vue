76212c3ba78cbf47b706b85ca16421ac543723e9
Trọng Tính <nguyentrongtinh280@gmailcom>
2026-01-24 09:33:10 +0700

    Lab 4: vuejs

---
+++ Lab44/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+//import Hello from "./components/Hello.vue";
+//import Bai3 from "./components/Bai3.vue";
+import Bai4 from "./components/Bai4.vue";
+//import DemoSlide4 from "./components/DemoSlide4.vue";
+</script>
+
+<template>
+  <Bai4 />
+</template>
+
+<style scoped></style>
---
+++ Lab44/src/components/DemoSlide4.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Hien thi noi dung</h1>
+  <h2>{{ message }}</h2>
+  <h2>{{ number + 5 }}</h2>
+  <h2>{{ sayHi() }}</h2>
+</template>
+
+<script setup>
+const message = "Xin chao vuejs";
+const number = 10;
+const sayHi = () => {
+  return "Day la mot phuong thuc";
+};
+</script>
