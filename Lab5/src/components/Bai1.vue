bff8a6f2b5f51db6d61c675f128c3faf93711f38
Trọng Tính <nguyentrongtinh280@gmailcom>
2026-02-03 10:56:20 +0700

    Lab5: Bài 1 (2 điểm): Làm quen cơ bản với sự kiện và binding động

---
+++ Lab5/src/components/Bai1.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="p-5">
+    <p :class="{ 'text-primary': isBlue, 'text-success': !isBlue }">
+      {{ message }}
+    </p>
+    <button class="btn btn-danger" @click="updateMessage">Bài 1</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const message = ref("PHẦN I: Data binding và Reactivity trong VueJS");
+const isBlue = ref(false);
+
+const updateMessage = () => {
+  message.value = "Bài 1: Làm quen cơ bản với sự kiện và binding động";
+  isBlue.value = !isBlue.value;
+};
+</script>
