3696e7b73dfad60c4281f318d3c5d60bfd69f947
Trọng Tính <nguyentrongtinh280@gmailcom>
2026-02-03 15:31:43 +0700

    Lab6: Bài 2: Viết chương trình nhập một tháng bất kỳ và in ra mùa tương

--- Lab6/src/App.vue
+++ Lab6/src/App.vue
@@ -1,7 +1,8 @@
 <script setup>
 import Bai1 from "./components/Bai1.vue";
+import Bai2 from "./components/Bai2.vue";
 </script>
 
 <template>
-  <Bai1 />
+  <Bai2 />
 </template>
---
+++ Lab6/src/components/Bai2.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <h3>Nhập tháng của bạn:</h3>
+    <br />
+    <input type="number" v-model.number="month" /><br /><br />
+    <p v-if="month < 1 || month > 12" class="text-danger fw-bold">
+      Vui lòng nhập tháng từ 1 đến 12
+    </p>
+    <p v-else-if="month >= 1 && month <= 3" class="text- fw-bold">Mùa xuân</p>
+    <p v-else-if="month <= 6" class="text-primary fw-bold">Mùa hè</p>
+    <p v-else-if="month <= 9" class="text-primary fw-bold">Mùa thu</p>
+    <p v-else class="text-primary fw-bold">Mùa đông</p>
+  </div>
+</template>
+<script setup>
+import { ref } from "vue";
+const month = ref("1");
+</script>
