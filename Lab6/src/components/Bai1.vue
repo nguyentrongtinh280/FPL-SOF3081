4c741c8c0b8f8c9aa9663eeb4f0d22b81cc54eb1
Trọng Tính <nguyentrongtinh280@gmailcom>
2026-02-03 15:20:35 +0700

    Lab6: Bài 1 (2 điểm): Nhập vào điểm trung bình của học sinh và in ra xếp loại tương

---
+++ Lab6/src/components/Bai1.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h3 class="text-danger">Nhập điểm của bạn</h3>
+    <input type="number" v-model="score" min="0" max="10" />
+    <p v-if="score >= 9" class="text-success">Xuất sắc</p>
+    <p v-else-if="score >= 8" class="text-success">Giỏi</p>
+    <p v-else-if="score >= 6.5" class="text-success">Khá</p>
+    <p v-else-if="score >= 5" class="text-success">Trung bình</p>
+    <p v-else class="text-success">Yếu</p>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+const score = ref(0);
+</script>
