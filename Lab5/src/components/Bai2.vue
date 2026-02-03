12a87dabe38b31d5337c870c39c64ec802afa211
Trọng Tính <nguyentrongtinh280@gmailcom>
2026-02-03 11:05:56 +0700

    Lab5: Bài 2 (2 điểm): Two-way Data Binding với v-model

---
+++ Lab5/src/components/Bai2.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <input v-model="userInput" placeholder="Nhập họ và tên" /><br /><br />
+    <input v-model="tuoi" placeholder="Nhập tuổi" />
+    <br /><br />
+    <div class="bg-warning">
+      <h5>Thông tin</h5>
+      <p class="text-danger fw-bold">Họ và tên: {{ userInput }}</p>
+      <p class="text-danger fw-bold">Tuổi: {{ tuoi }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const userInput = ref("");
+const tuoi = ref("");
+</script>
