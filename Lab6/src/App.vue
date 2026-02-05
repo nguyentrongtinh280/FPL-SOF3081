da013aa965a1cd0095d248f1ec9f648b76169e4c
Trọng Tính <nguyentrongtinh280@gmailcom>
2026-02-05 10:31:23 +0700

    LAb 6: DEMO SLIDE 6, v-if, v-for, v-show

--- Lab6/src/App.vue
+++ Lab6/src/App.vue
@@ -3,8 +3,10 @@ import Bai1 from "./components/Bai1.vue";
 import Bai2 from "./components/Bai2.vue";
 import Bai4 from "./components/Bai4.vue";
 import Bai3 from "./components/Bai3.vue";
+
+import demo from "./components/demoSlide.vue";
 </script>
 
 <template>
-  <Bai3 />
+  <demo />
 </template>
---
+++ Lab6/src/components/demoSlide.vue
@@ -0,0 +1,75 @@
+<template>
+  <h1 class="text-danger">V show - 1</h1>
+  <br />
+  <div>
+    <button class="btn btn-warning" @click="toggleVisibility">
+      Hiển thị / ẩn
+    </button>
+  </div>
+  <p v-show="isVisible">Thông điệp được chuyển đổi bởi v-show</p>
+  <br /><br />
+  <h1 class="text-danger">V show - 2</h1>
+  <div class="container mt-5 col-12">
+    <h2 class="mb-4 text-primary">TOP SẢN PHẨM BÁN CHẠY</h2>
+    <ul class="list-group">
+      <li class="list-group-item" v-for="(product, index) in products">
+        <h4>{{ product.name }} - {{ product.price }} VND</h4>
+
+        <button @click="toggleDetails(index)" class="btn btn-danger">
+          {{ showDetails[index] ? "ẨN CHI TIẾT" : "XEM CHI TIẾT" }}
+        </button>
+        <p v-show="showDetails[index]" class="mt-2">
+          {{ product.description }}
+        </p>
+      </li>
+    </ul>
+  </div>
+  <br />
+  <h1 class="text-danger">v-if và v-for</h1>
+  <br />
+
+  <ul>
+    <template v-for="item in items" :key="item.id">
+      <li v-if="item.visible">{{ item.name }}</li>
+    </template>
+  </ul>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+//v show 1
+const isVisible = ref(false);
+const toggleVisibility = () => {
+  isVisible.value = !isVisible.value;
+};
+
+//v show 2
+
+const products = ref([
+  {
+    name: "Quần jean",
+    description: "Quần jean cao cấp",
+    price: 120000,
+  },
+  {
+    name: "Quần kaki",
+    description: "Quần kaki cao cấp",
+    price: 120000,
+  },
+]);
+
+const showDetails = ref(products.value.map(() => false));
+const toggleDetails = (index) => {
+  showDetails.value[index] = !showDetails.value[index];
+};
+
+//v-if and v-for
+const items = ref([
+  { id: 1, name: "TRÁI TÁO", visible: true },
+  { id: 2, name: "TRÁI MÍT", visible: false },
+  { id: 3, name: "TRÁI CAM", visible: true },
+  { id: 3, name: "TRÁI BƠ", visible: false },
+  { id: 3, name: "TRÁI SẦU RIÊNG", visible: true },
+]);
+</script>
