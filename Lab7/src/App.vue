07430d6615e04861621d871fe2d9b977286d367f
Trọng Tính <nguyentrongtinh280@gmailcom>
2026-02-05 11:16:20 +0700

    Lab7: Bài 1: Tạo ứng dụng quản lý công việc. @submit.prevent

---
+++ Lab7/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Bai1 from "./components/Bai1.vue";
+// import Bai2 from "./components/Bai2.vue";
+// import Bai4 from "./components/Bai4.vue";
+// import Bai3 from "./components/Bai3.vue";
+
+// import demo from "./components/demoSlide.vue";
+</script>
+
+<template>
+  <Bai1 />
+</template>
---
+++ Lab7/src/components/Bai1.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="container mt-2">
+    <div class="card shadow-sm">
+      <div class="card-body">
+        <h3 class="text-primary fw-bold text-center">QUẢN LÝ CÔNG VIỆC</h3>
+        <br />
+        <form @submit.prevent="addList">
+          <div class="mb-3 text-start">
+            <label class="form label fw-bold">Tên công việc</label><br />
+            <input
+              type="text"
+              class="form-control"
+              v-model="newToDo"
+              placeholder="Nhập tên công việc"
+            />
+          </div>
+          <button type="submit" class="btn btn-success">Thêm công việc</button>
+        </form>
+        <ul class="list-group mt-4">
+          <li
+            class="list-group-item d-flex justify-content-between align-items-center"
+            v-for="(job, index) in jobs"
+            :key="index"
+          >
+            {{ job }}
+            <button class="btn btn-danger btn-sm" @click="remove(index)">
+              Xóa
+            </button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const newToDo = ref("");
+const jobs = ref(["Ăn sáng", "Đi học", "Học tiếng anh"]);
+const addList = () => {
+  if (newToDo.value.trim()) {
+    jobs.value.push(newToDo.value.trim());
+    newToDo.value = "";
+  }
+};
+
+const remove = (index) => {
+  if (confirm("Bạn có chắc muốn xóa công việc này khỏi danh sách?")) {
+    jobs.value.splice(index, 1);
+  }
+};
+</script>
